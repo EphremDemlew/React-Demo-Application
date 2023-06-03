96a2368f38e6cb303170ea168b44879b8287d580
Ephrem <ephyaabb@gmail.com>
2023-06-03 16:49:51 +0300

    added the cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Nav from "./components/Nav";
 import useData from "./useData";
+import Cards from "./components/Cards";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,9 +17,23 @@ function App() {
   return (
     <>
       <Nav />
-      <div className="py-10 px-20 font-semibold w-full h-full bg-red-200">
+      <div className="py-10 px-20 font-semibold w-full h-full ">
+        <h1 className="text-5xl py-10 text-teal-950 border-b-2 mb-10">
+          Welcome to the library of games!!
+        </h1>
         {isLoading && <h1 className="px-20 py-5 ">Loading...</h1>}
-        {data && <h1> Herray!! the data is here and is </h1>}
+        {data && (
+          <div className="aspect-video grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 mt-10">
+            {data.map((game) => (
+              <Cards
+                key={game.id}
+                title={game.name}
+                imageUrl={game.background_image}
+                hasData={true}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,19 @@
-function Cards() {
-  return <div>Cards</div>;
+function Cards({ title, imageUrl, hasData }) {
+  return (
+    <>
+      {hasData ? (
+        <div className="rounded flex flex-col gap-5 ">
+          <h1 className="text-xl font-semibold">{title}</h1>
+          <img src={imageUrl} alt={title} className="rounded" />
+        </div>
+      ) : (
+        <div className="rounded flex flex-col gap-5 bg-gray-100">
+          <h1 className="text-xl font-semibold animate-pulse w-44 h-6"></h1>
+          <img src={imageUrl} alt={title} className="rounded" />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Cards;
